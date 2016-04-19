6bab662dc830d5af474b48baa1a65cfe59ab4d82
Bill West <william.a.west@gmail.com>
2016-04-18 23:42:56 -0400

    Add working login button

--- js/fitLogin.js
+++ js/fitLogin.js
@@ -1,14 +1,44 @@
 var config = require('./config');
+var authButton = require('../html/authButton.html');
+var $ = require('jquery-browserify');
 
-module.exports = function (gapi) {
+function authorize(gapi, opts) {
+	options = opts !== undefined ? opts : {};
+	immediate = options.immediate !== undefined ? options.immediate : false;
+
         console.log("INFO: authorizing with google...");
-        gapi.auth.authorize({
+
+	params = {
                 client_id: config.gapi.clientId,
                 scope:     config.gapi.scopes,
-                immediate: false
-        },
-        function(auth_result) {
-                // TODO - auth result was fail, auth button retry checkAuth with immediate: false
-                console.log(auth_result);
-        });
+                immediate: immediate
+	};
+
+        gapi.auth.authorize(params, processAuthResult);
+}
+
+function authorizeClicked() {
+	authorize(gapi, {immediate: false});
+}
+
+function processAuthResult(authResult) {
+	var button = $('#fit-hr-zones-auth');
+
+	if(authResult.status.signed_in) {
+		console.log('disabling login button');
+		button.prop('disabled', true);
+		button.html('Logged in');
+	} else {
+		console.log('enabling login button');
+		button.prop('disabled', false);
+		button.html('Authorize');
+	}
+}
+
+module.exports = function (gapi) {
+	$('header').append(authButton);
+	var button = $('#fit-hr-zones-auth');
+	button.click(authorizeClicked);
+
+	authorize(gapi, {immediate: true});
 };
